c2d6f77935ef594b377c69871ccc492996c92fb4
BernardSapida <BernardSapida1706@gmail.com>
2022-11-02 20:37:54 +0800

    icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="/node_modules/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <link rel="icon" href="images/Logo/bg.jpg">
+        <link rel="icon" href="images/Logo/bs.jpg">
         <style lang="scss" scoped>
             html {  
                 user-select: none;
